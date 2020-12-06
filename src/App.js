cef4f94558d971abf4fd881744a48613245ae7be
Ivanchenko Dmitriy <aspek777@gmail.com>
2020-12-06 16:23:03 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">Hi!<br/>
+        My name is Dmitriy Ivanchenko.<br/>
+        I’m a JavaScript developer.
+      </div>
+      <div className="sub">TypeScript, Node.js, React</div>
+      <div className="contacts">Contact me on via <a href="mailto:info@ivanchenko-d.ru">email</a> or <a
+        href="https://t.me/dmitriy_ivanchenko">Telegram</a>.</div>
+    </div>
+  );
+}
+
+export default App;
